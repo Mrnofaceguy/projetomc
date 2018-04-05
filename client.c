9ad07938bedf457b9a627e902fb95d4b69f3cac6
davidorg1 <38101920+davidorg1@users.noreply.github.com>
2018-04-05 12:38:53 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-
-void main(){
-  printf("-Menu inicial-");
+int menuderegisto()
+{
+  printf("**Menu de registo**");
   printf("1) Login /Autenticação  ");
-  printf("2) Pedido de registo");
-  printf("3) Sair");
-  int x;
-  scanf("%d", &x);
-  switch (x){
-  case 1:printf("");
-  case 2:
-  case 3: exit;
-  default:
-  }
+  printf("2) Pedido de registo ");
+  printf("3) Sair ");
 }
-int verification(str[],str[]){
-}
-int login(){
-  printf(
+
+int menu()
+{
+  printf("**Menu**");
+  printf("1) Ver feed");
+  printf("2) Gerir Tópicos/Utilizadores");
+  printf("3) Ver Estatísticas ");
+  printf("4) Subscrever tópico");
+  printf("5) Publicar num tópico");
+  printf("6) Gerir Lista de Subscrições");
+  printf("7) Ver Estatísticas");
+  printf("8) Logout");
+}  
