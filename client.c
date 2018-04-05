c29c3aec4060cd6bd9dcb7d9636d2681ee6399ea
Mrnofaceguy <38101523+Mrnofaceguy@users.noreply.github.com>
2018-04-05 11:39:36 +0000

    Update client.c

--- client.c
+++ client.c
@@ -3,12 +3,13 @@
 void main(){
   printf("-Menu inicial-");
   printf("1) Login /Autenticação  ");
-  printf("2) Pedido de registo");
+  printf("2) Menu de registo");
   printf("3) Sair");
+  nenu();
   int x;
   scanf("%d", &x);
   switch (x){
-  case 1:printf("");
+  case 1:login();
   case 2:
   case 3: exit;
   default:
@@ -17,4 +18,10 @@ void main(){
 int verification(str[],str[]){
 }
 int login(){
-  printf(
+  char nome[];
+  char passw[];
+  printf("Escreva o seu nome de utilizador:");
+  scanf("%c",&nome[]);
+  printf("Escreva a sua password");
+  scanf
+}
