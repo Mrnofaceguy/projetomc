cf0b6ebc5e50e251bfef5ca06f25ef2896877aad
Mrnofaceguy <38101523+Mrnofaceguy@users.noreply.github.com>
2018-04-05 12:19:57 +0100

    Add files via upload

---
+++ client.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main(){
+  printf("-Menu inicial-");
+  printf("1) Login /Autenticação  ");
+  printf("2) Pedido de registo");
+  printf("3) Sair");
+  int x;
+  scanf("%d", &x);
+  switch (x){
+  case 1:printf("");
+  case 2:
+  case 3: exit;
+  default:
+  }
+}
+int verification(str[],str[]){
+}
+int login(){
+  printf(
