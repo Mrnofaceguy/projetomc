316cb68df2a5112bfb092d8112c8b3b96a190350
davidorg1 <38101920+davidorg1@users.noreply.github.com>
2018-04-26 12:41:36 +0100

    Add files via upload

--- SupCliente.c
+++ SupCliente.c
@@ -3,6 +3,8 @@
 #include<ctype.h>
 #include<stdlib.h>
 
+
+
 void
 menu_de_registo()
 {
@@ -11,6 +13,34 @@ menu_de_registo()
         printf("2) Pedido de registo\n");
         printf("0) Sair\n");
 }
+
+
+/*
+void
+Menu()
+{
+        printf("1) Ver feed\n");
+        printf("2) Ver tópicos\n");
+        printf("3) Procurar tópicos mais ativos\n");
+        printf("4) Subscrever tópico\n");
+        printf("5) Publicar num tópico\n");
+        printf("6) Gerir Lista de Subscrições\n");
+        printf("7) Ver Estatísticas\n");
+        printf("8) Logout\n");
+        switch (input)
+        {
+                case 1:
+                        Login();
+                case 2:
+                        //Pedido_de_registo();
+                case 0:
+                        return 0;
+                default:
+                        printf("A opção escolhida nao existe.Tente outra vez\n");
+                        main();
+        }
+}
+
 
 int
 Login()
@@ -22,11 +52,14 @@ Login()
         char s[20];
 
         FILE *fp;
-        fp = fopen("~/Documents/project_lab/SupCliente_assets/info_util.txt","r");
+        fp = fopen("/net/areas/homes/up201704695/Documents/project_lab/SupCliente_assets/Dados_Login.txt","r");
+        if(!fp)
+                printf ("Erro na abertura do arquivo.");
         printf("Insira o username \n"); scanf("%s", username);
         printf("Insira a password \n"); scanf("%s",passwd);
         while (fgets(s,20,fp))
         {
+
                 int i = 0;
                 for (i = 0; s[i] != ';'; ++i)
                         temp1[i] = s[i];
@@ -86,11 +119,40 @@ main()
                 case 1:
                         Login();
                 case 2:
-                        //Pedido_de_registo();
+                        Pedido_de_registo();
                 case 0:
                         return 0;
                 default:
                         printf("A opção escolhida nao existe.Tente outra vez\n");
                         main();
         }
+}
+
+int
+Pedido_de_registo()
+{
+        char newusername[20];
+        char newpasswd[20];
+        char continuar = 0;
+        printf("Insira um username (menos de vinte caracteres)\n"); scanf("%s", newusername);
+        printf("Insira uma password (menos de vinte caracteres)\n"); scanf("%s\n", newpasswd);
+        FILE *fp;
+        fp = fopen("/net/areas/homes/up201704695/Documents/project_lab/Pedido_de_registo.txt","a");
+        if(!fp)
+                printf ("Erro na abertura do arquivo.");
+        fprintf(fp, "%s;%s;", newusername, newpasswd);
+        fclose(fp);
+        printf("O seu pedido está a ser processado\n");
+
+        printf("Se desejar ir para o menu de registo escolha a opção 1)\n Se desejar sair escolha outra opção qualquer\n");
+        fflush(stdout);
+
+        scanf("%c\n", &continuar);
+        if(continuar == '1')
+        {
+                main();
+
+        }
+        return 0;
+
 }
